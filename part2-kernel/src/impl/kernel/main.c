cf9e140549d8f3b6d7ab5ad8e095700ba720e526
David1011-T <datorresve@uide.edu.ec>
2026-06-20 17:26:50 +0000

    Modify to put all group members names

--- part2-kernel/src/impl/kernel/main.c
+++ part2-kernel/src/impl/kernel/main.c
@@ -1,24 +1,24 @@
 #include "print.h"
 
 void kernel_main() {
-    // 1. Limpiar la pantalla de los residuos de GRUB
+    //Limpiar la pantalla de los residuos de GRUB
     print_clear();
     
-    // 2. Elegir colores (ej: texto amarillo sobre fondo negro)
+    //Elegir colores
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     
-    // 3. Imprimir el texto requerido por la rúbrica
+    //Imprimir el texto 
     print_str("Welcome to 64-bit kernel\n");
     
     print_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK);
     print_str("\nSistema Operativo creado por el grupo de:\n");
     
-    // Puedes añadir los nombres del resto de tus amigos aquí abajo
+    //Nombres del grupo
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
     print_str("- David Torres\n");
-    print_str("- Integrante 2\n");
-    print_str("- Integrante 3\n");
-    print_str("- Integrante 4\n");
+    print_str("- Ivan Gualotuña\n");
+    print_str("- Andres Guaminga\n");
+    print_str("- Francis Acuña\n");
     
     print_set_color(PRINT_COLOR_LIGHT_CYAN, PRINT_COLOR_BLACK);
     print_str("\n[ OK ] Sistema iniciado correctamente en modo 64 bits.\n");
